540998cbd0ab16812e026941741ed82927040e62
h-behgam <h.behgam@gmail.com>
2023-11-12 15:51:00 +0100

    remove bug

--- src/Components/Buttons/OperationButton.js
+++ src/Components/Buttons/OperationButton.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { action } from "../Form/Form";
 
-function OperationButton({ operation, className }) {
-  return <button className={className ? className + " operator" : "operator"}>{operation}</button>;
+function OperationButton({ operation, className, dispatch }) {
+  return (
+    <button
+      className={className ? className + " operator" : "operator"}
+      onClick={() => {
+        dispatch({ type: action.ChoseOperation, payload: { operation } });
+      }}
+    >
+      {operation}
+    </button>
+  );
 }
 
 export default OperationButton;
