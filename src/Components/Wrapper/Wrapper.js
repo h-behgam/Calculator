b84161aff214f69355ad096051e3d97ab46f2777
h-behgam <h.behgam@gmail.com>
2023-11-12 12:20:11 +0100

    Create Wrapper.js

---
+++ src/Components/Wrapper/Wrapper.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Form from "../Form/Form";
+
+function Wrapper() {
+  return (
+    <div className="container">
+      <div className="calculator">
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default Wrapper;
